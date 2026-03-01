8ebf13c8b5206786c9c0825a0793c890aae382f8
Will Green <will.green98@hotmail.com>
2026-03-01 21:22:50 +0000

    chore(module): correctly export modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,18 @@
 module org.bgw.tetrisfx {
-        requires javafx.controls;
-        requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
 
-        exports org.bgw.tetrisfx;
-        opens   org.bgw.tetrisfx to javafx.fxml;
+    exports org.bgw.tetrisfx;
 
-        exports org.bgw.tetrisfx.controller;
-        opens   org.bgw.tetrisfx.controller to javafx.fxml;
+    opens org.bgw.tetrisfx to
+            javafx.fxml;
 
-        exports org.bgw.tetrisfx.model;
-        exports org.bgw.tetrisfx.view;
-        exports org.bgw.tetrisfx.service;
-        }
+    exports org.bgw.tetrisfx.controller;
+
+    opens org.bgw.tetrisfx.controller to
+            javafx.fxml;
+
+    exports org.bgw.tetrisfx.model;
+    exports org.bgw.tetrisfx.view;
+    exports org.bgw.tetrisfx.service;
+}
